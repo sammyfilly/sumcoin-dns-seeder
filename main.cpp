51fd61b3e2a25b3342dfec3f4b377333a8e70f22
sumcoinlabs <sumcoinindex@gmail.com>
2023-04-24 13:34:07 -0600

    pch message params

--- main.cpp
+++ main.cpp
@@ -418,7 +418,7 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dnsseed.sumcoin.org", "dnsseed.sumexplorer.com", ""};
+static const string mainnet_seeds[] = {"127.0.0.1", "dnsseed.sumcoin.org", "dnsseed.sumexplorer.com", ""};
 static const string testnet_seeds[] = {"testnet-seed.alexykot.me",
                                        "testnet-seed.bitcoin.petertodd.org",
                                        "testnet-seed.bluematt.me",
@@ -480,10 +480,10 @@ int main(int argc, char **argv) {
   bool fDNS = true;
   if (opts.fUseTestNet) {
       printf("Using testnet.\n");
-      pchMessageStart[0] = 0x0b;
-      pchMessageStart[1] = 0x11;
-      pchMessageStart[2] = 0x09;
-      pchMessageStart[3] = 0x07;
+      pchMessageStart[0] = 0xf1;
+      pchMessageStart[1] = 0xe6;
+      pchMessageStart[2] = 0xf9;
+      pchMessageStart[3] = 0xa2;
       seeds = testnet_seeds;
       fTestNet = true;
   }
